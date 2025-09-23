f8d3d6b78bd0ad137b58dc94fa43ccf7bed51219
tuomas <itupu002@edu.xamk.fi>
2025-09-23 13:46:35 +0300

    joooo

---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Choose an operation: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+            Console.ReadLine();
+        }
+    }
+}
